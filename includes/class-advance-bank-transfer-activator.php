b80349d4f6c909b8fa5530e94a222fc3f5d08b2e
Ashutosh Verma <47808689+LoganHunts@users.noreply.github.com>
2020-12-01 12:24:43 +0530

    initialized biolerplate

---
+++ includes/class-advance-bank-transfer-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.linkedin.com/in/ashutosh-verma-8609aa15b/
+ * @since      1.0.0
+ *
+ * @package    Advance_Bank_Transfer
+ * @subpackage Advance_Bank_Transfer/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Advance_Bank_Transfer
+ * @subpackage Advance_Bank_Transfer/includes
+ * @author     Ashutosh Verma <vermaa947@gmail.com>
+ */
+class Advance_Bank_Transfer_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ public/class-advance-bank-transfer-public.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * @link       https://www.linkedin.com/in/ashutosh-verma-8609aa15b/
+ * @since      1.0.0
+ *
+ * @package    Advance_Bank_Transfer
+ * @subpackage Advance_Bank_Transfer/public
+ */
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the public-facing stylesheet and JavaScript.
+ *
+ * @package    Advance_Bank_Transfer
+ * @subpackage Advance_Bank_Transfer/public
+ * @author     Ashutosh Verma <vermaa947@gmail.com>
+ */
+class Advance_Bank_Transfer_Public {
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $plugin_name    The ID of this plugin.
+	 */
+	private $plugin_name;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $plugin_name       The name of the plugin.
+	 * @param      string    $version    The version of this plugin.
+	 */
+	public function __construct( $plugin_name, $version ) {
+
+		$this->plugin_name = $plugin_name;
+		$this->version = $version;
+
+	}
+
+	/**
+	 * Register the stylesheets for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_styles() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Advance_Bank_Transfer_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Advance_Bank_Transfer_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/advance-bank-transfer-public.css', array(), $this->version, 'all' );
+
+	}
+
+	/**
+	 * Register the JavaScript for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_scripts() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Advance_Bank_Transfer_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Advance_Bank_Transfer_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/advance-bank-transfer-public.js', array( 'jquery' ), $this->version, false );
+
+	}
+
+}
